e59de2a1f4d216809418894ddafe8eec029a374c
thexwool <allan.dsm95@gmail.com>
2021-12-10 20:06:59 -0300

    commit

--- estruturas de controle/ifElseIf.js
+++ estruturas de controle/ifElseIf.js
@@ -2,7 +2,7 @@ Number.prototype.entre = function (inicio, fim){
     return this >= inicio && this <= fim
 }
 
-imprimirResultado = function(nota){
+function imprimirResultado(nota){
     if(nota.entre(9, 10)){
         console.log('Quadro de Honra')
     }else if(nota.entre(7, 8.99)){
